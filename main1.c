9a97afd8aaeecf0ff8e49f5185f591b54e195e41
William Ratcliff <william.ratcliff@gmail.com>
2008-07-14 21:37:02 +0000

    working on transferring the atom info from python to C

--- main1.c
+++ main1.c
@@ -29,7 +29,7 @@ struct interaction
 
 struct atom
 {
-//       float s[3];   //this notation is cuasing problems when i try to assign it a value for soe reason
+//       float s[3];   //this notation is cuasing problems when i try to assign it a value for some reason
 //       float pos[3];
        int numInteractions;
 //       struct interaction interactions[];
@@ -123,6 +123,139 @@ void simulate(struct atom atoms[], int numAtoms, float jMatrices[][3][3], int k,
              T = T*tFactor;
      }
 }
+
+//test
+/*struct atomArray
+{
+       struct atom atomArr[];
+}
+
+*/
+
+/*
+typedef struct {
+  double *Spp, *Smm, *Spm, *Smp ;
+  double *Epp, *Emm, *Epm, *Emp ;
+  double *R ;
+} PBoutdata ;
+*/
+
+/*
+typedef struct
+{
+       int num;
+       int ints[];
+} testStruct;
+
+int basicTest(testStruct  *struc)
+{
+    int i;
+    printf("%d\n", struc->num);
+    for( i = 0; i < struc->num; i++)
+    { 
+        printf("i=%d struc.ints=%d\n",i, struc->ints[i]);
+    }
+    return struc->num + 1;
+}
+
+typedef struct
+{
+        int num;
+        testStruct *structs[];
+}ArrayStruct;
+
+void test2(ArrayStruct *struc)
+{
+     int i;
+     int j;
+     for( j = 0; j < struc->num; j++)
+     {
+              for( i = 0; i < struc->structs[j]->num; i++)
+              { 
+                printf("i=%d struc.ints=%d\n",i, struc->structs[j]->ints[i]);
+              }
+     }
+}
+
+int test(struct atom atoms[], int numAtoms)
+{
+//    struct atom *atoms = atomArray.atomArr;
+    int i = 0;
+    int j;
+    struct atom a;
+    for(i = 0; i < numAtoms; i++)
+    {
+      a = atoms[i];
+      printf("%d) NumInteractions: %d\n",i,a.numInteractions);
+      printf("%d) s[0]: %d\n",i,a.s[0]);
+      printf("%d) s[1]: %d\n",i,a.s[1]);
+      printf("%d) s[2]: %d\n",i,a.s[2]);
+      printf("%d) pos[0]: %d\n",i,a.pos[0]);
+      printf("%d) pos[1]: %d\n",i,a.pos[1]);
+      printf("%d) pos[2]: %d\n",i,a.pos[2]);
+      for(j = 0; j < a.numInteractions; j++)
+      {
+            printf("%d,%d) NumInteractions: %d\n",i,j, a.interactions[j].jMatrixIndex);
+            printf("%d,%d) NumInteractions: %d\n",i,j, a.interactions[j].otherAtomIndex);
+      }
+    }
+    
+    return 0;
+}
+
+*/
+
+typedef struct
+{
+        int length;
+        int interactions[];
+}interactionsStruct;
+
+typedef struct
+{
+        int number;
+        float sArray[][3][3];
+        interactionsStruct interactionsArray[];
+}atomArrayStruct;  
+
+
+
+
+void test3(int k , float tMax, float tMin, float tFactor, atomArrayStruct atoms)
+{
+     return
+}
+
+
+typedef float spintype[3];
+
+typedef struct{
+        int *interaction_matrix;
+        int *nbr_list;
+        spintype spin; 
+        } Atom;
+        
+void new_atom(int n){
+     
+     Atom *a;
+     a=malloc(n*sizeof(Atom));
+     return a;
+     }
+
+void del_atom(atom *p) {free(p)}
+
+void set_atom(atom *p, int *mat, int* neighbors, spin* spin){
+     p[k].matrix=mat;
+     p[k].neighbor=neighbor;
+     
+     
+     
+     }
+
+
+
+
+
 
 
 int main(int argc, char *argv[])
