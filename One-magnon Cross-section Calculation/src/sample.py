b7caa5a6693049a644a5abdde69b46147a52a31c
William Ratcliff <william.ratcliff@gmail.com>
2009-07-01 20:02:48 +0000

    This is old and outdated. Removing

--- One-magnon Cross-section Calculation/src/sample.py
+++
@@ -1,7 +0,0 @@
-import sympy as sp
-from sympy import exp,cos,I
-x,y,z,a,b,c = sp.symbols('xyzabc')
-alist = [a*b*exp(x*I - y),(a-c)*exp(x**2+y*I)]
-
-n = sp.Symbol('n',integer=True)
-sp.sum(alist[n],(n,0,len(alist)))
