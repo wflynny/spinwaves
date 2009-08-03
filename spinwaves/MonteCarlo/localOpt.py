4dc7c0453285427674dae780e6368148d8f89cdc
William Ratcliff <william.ratcliff@gmail.com>
2009-08-03 15:26:49 +0000

    Documented localOpt.py

--- spinwaves/MonteCarlo/localOpt.py
+++ spinwaves/MonteCarlo/localOpt.py
@@ -2,6 +2,9 @@
 Created on Jul 16, 2009
 
 @author: tsarvey
+
+Updated on Jul 30, 2009
+@author: wflynn
 '''
 import copy
 #from ctypes import c_float, c_int
@@ -87,8 +90,10 @@ def optimizeSpins(interactionFile, inFile, outFile):
     m = mpfit(hamiltonian, p0, parinfo = parinfo)
     return m.params
 
+
+
 def gen_Jij(atom_list,jmats):
-    """ Creates a scipy bsr sparse array """
+    """ Creates a scipy bsr sparse array of the Jij interaction matrices"""
     N_atoms = len(atom_list)
     jij_values = []
     jij_columns = []
@@ -137,6 +142,7 @@ def gen_Jij(atom_list,jmats):
     return jij
 
 def gen_spinVector(atom_list):
+    """ From an atom_list, this method returns a numpy array of the atoms' spinvectors"""
     N_atoms = len(atom_list)
     vect_list = []
     for i in range(N_atoms):
@@ -147,6 +153,7 @@ def gen_spinVector(atom_list):
     return np.array(vect_list)
 
 def gen_anisotropy(atom_list):
+    """ From an atom_list, this method returns a numpy array of the atoms' anisotropy terms"""
     N_atoms = len(atom_list)
     anis_vect = []
     for i in range(N_atoms):
@@ -157,62 +164,105 @@ def gen_anisotropy(atom_list):
     return anis_vect
 
 def calculate_Ham(sij, anis, jij, dsij = None):
+    """ 
+    Method the local optimizer uses
+    Takes:
+    - spin vector of the form [Sx1,Sy1,Sz1,Sx2,Sy2,Sz2,...]
+    - anisotropy vector of the form [[Dx1,Dy1,Dz1],[Dx2,Dy2,Dz2],...]
+    - jij sparse matrix of interaction matrices
+    - dsij derivative of the spin vector. see the deriv() method in local optimizer
+    Returns:
+    - Hamilitonian Expression
+    """
     
-    sijT =sij.T    #sij.reshape((1,sij.shape[0]))
+    sijT =sij.T
     
+    # Normal mode
+    # sij.T * jij * sij
     if dsij == None:
         res1 = sijT * jij
         Hij = np.dot(res1,sij).flat[0]
         Ham = - Hij - np.dot(anis, sij**2)
+    # Derivative mode
+    # dsij * jij * sij
     else:
         res1 = dsij * jij
         Hij = np.dot(res1,sij)
         Ham = - Hij - np.matrix(np.dot((dsij**2),anis)).T
 
-
     return Ham
 
 def local_optimizer(interfile, spinfile):
+    """
+    Given an interaction file and a spin file, this optimizer will uses readfiles.readfiles to populate 
+    an atom list as well as return the interaction matrices. Given that information, it will recalculate
+    the hamiltonian and attempt to minimize it as a function of theta and phi. The annealing will get the
+    spins close to the global minimum and this method makes sure the spins are aligned. This routine uses
+    only python instead of C. 
+    """
+
+    # Get atoms list, jmats from the interaction and spin files
     atom_list, jnums, jmats,N_atoms_uc=rf.readFiles(interfile,spinfile,allAtoms=True)
     N_atoms = len(atom_list)
+    # Generate the Jij and anisotropy arrays
     Jij = gen_Jij(atom_list,jmats)
     anis = gen_anisotropy(atom_list)
+    # Get the spin magnitudes from the atoms in atom list
     spin_mags = []
     for i in range(len(atom_list)):
         spin_mags.append(atom_list[i].spinMagnitude)
     spin_mags = np.array(spin_mags)
     
+    # hamiltonian method
     def hamiltonian(p, Jij = None, spins = None, anis = None):
+        """ Computes the hamiltonian given a list a thetas and phis"""
+        # Thetas are the first half, phis the second half
         theta = p[:len(p)//2]
         phi = p[len(p)//2:]
 
+        # Sx,Sy,Sz
         Sx = spins*cos(theta)*cos(phi)
         Sy = spins*cos(theta)*sin(phi)
         Sz = spins*sin(theta)
 
+        # Array of spin vectors for each atom. Reshape it. Calculate hamiltonian with it and return the hamiltonian. 
         Sij = np.array([Sx,Sy,Sz])
         Sij = Sij.T.reshape(1,3*len(p)//2).T
         result = calculate_Ham(Sij, anis, Jij)
 
         return result
     
+    # derivative of the hamiltonian
     def deriv(p, Jij = None, spins = None, anis = None):
-        whole = len(p)
+        """ Computes the derivative of the hamiltonian with respect to each theta and then each phi"""
+        # Thetas are the first half, phis the second half
         half = len(p)/2
         theta = p[:half]
         phi = p[half:]        
 
+        # Sx,Sy,Sz
         Sx = spins*cos(theta)*cos(phi)
         Sy = spins*cos(theta)*sin(phi)
         Sz = spins*sin(theta)
 
+        # dSx/dtheta,dSy/dtheta,dSz/dtheta
         Sxt = -spins*sin(theta)*cos(phi)
         Syt = -spins*sin(theta)*sin(phi)
         Szt = spins*cos(theta)
+        # dSx/dphi,dSy/dphi,dSz/dphi
         Sxp = -spins*cos(theta)*sin(phi)
         Syp = spins*cos(theta)*cos(phi)
         Szp = 0*cos(theta)
         
+        # Makes an array of the derivatives with respect to theta, then another with respect to phi
+        # (dSx1/dtheta1,dSy1/dtheta1,dSz1/dtheta1
+        # (                                      dSx2/dtheta2,dSy2/dtheta2,dSz2/dtheta2
+        # (                                                                             ...
+        # Similarly for phis
+        # Then we multiply this by Jij which yields a half by 3*half array. We then multiply this by
+        # the Sij vector which finally yields a half by 1 array. Thus
+        # dSijt * Jij * Sij -> [dE/dtheta1, dE/dtheta2, dE/dtheta3, ...]
+        # and similarly for phi
         dSijt = np.zeros((half,3*half))
         dSijp = np.zeros((half,3*half))
         dSijt[range(half),range(0,3*half,3)]=Sxt
@@ -222,16 +272,19 @@ def local_optimizer(interfile, spinfile):
         dSijp[range(half),range(1,3*half,3)]=Syp
         dSijp[range(half),range(2,3*half,3)]=Szp
         
+        # Standard Sij spin vector we've been using
         Sij = np.array([Sx,Sy,Sz])
         Sij = Sij.T.reshape(1,3*len(p)//2).T
+        # Calculate a hamiltonian for both theta and phi
         rest = calculate_Ham(Sij,anis,Jij,dsij = dSijt)
         resp = calculate_Ham(Sij,anis,Jij,dsij = dSijp)
+        # Concatenate the two and return the result
         result = np.concatenate((np.array(rest),np.array(resp)))
-        
 
         return result.T
 
-    #populate initial p list
+    # populate initial p list
+    # returns a numpy array of all the thetas followed by all the phis
     thetas = []
     phis = []
     for i in range(N_atoms):
@@ -247,6 +300,7 @@ def local_optimizer(interfile, spinfile):
         phis.append(phi)
     p0 = np.array(thetas+phis)
     
+    # define the limits parameter list
     limits = []
     for i in range(len(p0)):
         if i < len(p0)//2:#theta
@@ -254,17 +308,25 @@ def local_optimizer(interfile, spinfile):
         else:#phi
             limits.append((0,pi))
     
-    m = fmin_l_bfgs_b(hamiltonian, p0, fprime = deriv, args = (Jij, spin_mags, anis), bounds = limits)#, approx_grad = True)
+    # call minimizing function
+    m = fmin_l_bfgs_b(hamiltonian, p0, fprime = deriv, args = (Jij, spin_mags, anis), bounds = limits)
+
+    # grab returned parameters
+    # thetas are the first half of the list, phis are the second half
     pout=m[0]
     theta=pout[0:len(pout)/2]
     phi=pout[len(pout)/2::]
+    # recreate sx,sy,sz's
     sx=spin_mags*cos(theta)*cos(phi)
     sy=spin_mags*cos(theta)*sin(phi)
     sz=spin_mags*sin(theta)
     
+    # return spin vector array
     return np.array([sx,sy,sz]).T
 
+
 def optimization_driver(interfile, spinfile):
+    """ runs the local optimization routine given just an interactionfile and spinfile. this should be called by the GUI """
     atom_list, jnums, jmats, N_atoms_uc = rf.readFiles(interfile,spinfile,allAtoms=True)
     N_atoms = len(atom_list)
     jij = gen_Jij(atom_list,jmats)
@@ -286,7 +348,7 @@ if __name__ == '__main__':
     atom_list, jnums, jmats,N_atoms_uc=rf.readFiles(interfile,spinfile,allAtoms=True)
     N_atoms = len(atom_list)
 
-    jij = gen_Jij(atom_list)
+    jij = gen_Jij(atom_list, jmats)
     anis = gen_anisotropy(atom_list)
     sij = gen_spinVector(atom_list)
     Ham = calculate_Ham(sij, anis, jij)
